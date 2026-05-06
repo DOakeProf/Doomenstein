397675b275b4a5a34b1b08f0f60fb5a4e755e91b
DOakeProf <danieloake02@gmail.com>
2026-05-06 17:17:48 -0600

    <Opened by P4VS>
    [git-p4: depot-paths = "//depot/C35/Students/doake/SD/Doomenstein/": change = 461266]

--- Code/Game/AI.hpp
+++ Code/Game/AI.hpp
@@ -2,17 +2,31 @@
 
 #include "Game/Controller.hpp"
 
+enum class AIType
+{
+	NONE = -1,
+	MELEE,
+	RANGED,
+	FLYING_MELEE,
+	FLYING_RANGED,
+	NUM_TYPES
+};
+
 class AI : public Controller
 {
 public:
 	AI() = default;
-	AI(Map* map);
+	AI(Map* map, AIType aiType);
 	~AI() = default;
 
 	ActorHandle* m_targetActorHandle = nullptr;
 
+	AIType m_type;
+
 	void DamagedBy(ActorHandle* otherActor);
 	void Update() override;
+	void Update_Melee();
+	void Update_Ranged();
 
 	void Update_FindTargetActor();
 
